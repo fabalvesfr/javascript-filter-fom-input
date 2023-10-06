f02b3d24cae41e88d548c4aa66507bdc9a1e098c
fabalvesfr <fabio9162@gmail.com>
2023-10-06 16:08:07 +0200

    Replaced Promise and Resolve/Reject for fetch

--- index.js
+++ index.js
@@ -4,67 +4,62 @@ const filteredNames = document.getElementById("filtered-names"); // DOM containe
 
 // Sourcing names from API
 apiURL = "https://jsonplaceholder.typicode.com/users";
-const apiRequest = new XMLHttpRequest();
 
-function getRequest() {
-  return new Promise((resolve, reject) => {
-    apiRequest.open("GET", apiURL);
-
-    apiRequest.onreadystatechange = () => {
-      if (apiRequest.readyState === 4) {
-        if (apiRequest.status === 200) {
-          resolve(JSON.parse(apiRequest.response));
-        } else {
-          reject(JSON.parse(apiRequest.response));
-        }
-      }
-    };
-    apiRequest.send();
-  });
-}
-
-// Using Async/Await to handle the asynchronicity of the HTTP request operation
-async function getNames() {
-  const response = await getRequest();
-  //populating an array with only the person's name and email address
-  const contactsList = [];
-  for (let i = 0; i < response.length; i++) {
-    const contact = {
-      name: response[i].name,
-      email: response[i].email,
-    };
-    contactsList.push(contact);
+// New function to replace synchronous getRequest()
+async function getRequest() {
+  const response = await fetch(apiURL);
+  if (response.status != 200) {
+    throw new Error("Get request was not successful");
   }
-  nameInput.addEventListener("input", () => {
-    // Cleaning the filtered names on the browser before adding new ones
-    filteredNames.innerHTML = "";
-
-    // Grabbing the user's input and cleaning it
-    const cleanInputValue = nameInput.value.toLowerCase();
-
-    // Filtering the list according to what user has typed
-    filteredList = contactsList.filter((contact) =>
-      contact.name.toLowerCase().includes(cleanInputValue)
-    );
-
-    filteredList.forEach((contact) => {
-      // Creating DOM elements for each of the items in the filtered list
-      const container = document.createElement("div");
-      container.setAttribute("class", "filtered-name-container");
-
-      const filteredName = document.createElement("h3");
-      filteredName.textContent = contact.name;
-
-      const filteredEmail = document.createElement("p");
-      filteredEmail.textContent = contact.email;
-
-      // Appending the children DOM elements to their respective parent
-      container.appendChild(filteredName);
-      container.appendChild(filteredEmail);
-      filteredNames.appendChild(container);
-    });
-  });
+  return response.json();
 }
 
-// Calling the getNames() function to launch the HTTP Get request and start the filtering process on the user input
-getNames();
+async function displayNames() {
+  try {
+    const response = await getRequest();
+    //populating an array with only the person's name and email address
+    const contactsList = [];
+    for (let i = 0; i < response.length; i++) {
+      const contact = {
+        name: response[i].name,
+        email: response[i].email,
+      };
+      contactsList.push(contact);
+    }
+    nameInput.addEventListener("input", () => {
+      // Cleaning the filtered names on the browser before adding new ones
+      filteredNames.innerHTML = "";
+
+      // Grabbing the user's input and cleaning it
+      const cleanInputValue = nameInput.value.toLowerCase();
+
+      // Filtering the list according to what user has typed
+      let filteredList = contactsList.filter((contact) =>
+        contact.name.toLowerCase().includes(cleanInputValue)
+      );
+
+      // TODO: function displayData() : Enrober ce morceau final de code dans une fonction que je pourrai utiliser ailleurs
+      filteredList.forEach((contact) => {
+        // Creating DOM elements for each of the items in the filtered list
+        const container = document.createElement("div");
+        container.setAttribute("class", "filtered-name-container");
+
+        const filteredName = document.createElement("h3");
+        filteredName.textContent = contact.name;
+
+        const filteredEmail = document.createElement("p");
+        filteredEmail.textContent = contact.email;
+
+        // Appending the children DOM elements to their respective parent
+        container.appendChild(filteredName);
+        container.appendChild(filteredEmail);
+        filteredNames.appendChild(container);
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Calling the displayNames() function to launch the HTTP Get request and start the filtering process on the user input
+displayNames();
